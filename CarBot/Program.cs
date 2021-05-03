8a4f30a9a139d0a6567132b55ed2b2f800a3782a
Azatkzn <a.muhamadyarov@yandex.ru>
2021-05-03 20:06:32 +0300

    1.0.1

--- CarBot/Program.cs
+++ CarBot/Program.cs
@@ -29,7 +29,11 @@ namespace CarBot
 				Console.WriteLine(ex.Message);
 				Console.WriteLine(ex.StackTrace);
 				var path = Path.Combine(Directory.GetCurrentDirectory(), "log.txt");
+<<<<<<< HEAD
+				using (var stream = new StreamWriter(path, true))
+=======
 				using (var stream = new StreamWriter(path))
+>>>>>>> 2a3cc5e2ddfc9a09f81bb7204f3d2ee032ed813c
 				{
 					stream.WriteLine("{0}:  {1}", DateTime.Now, ex.Message);
 					stream.WriteLine("{0}:  {1}", DateTime.Now, ex.StackTrace);
